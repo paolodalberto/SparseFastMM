8a08de3a619a5d9997d97fadb1c8dc65949bb1db
paolo d'alberto <pdalberto@ninthdecimal.com>
2023-06-29 16:21:31 -0700

    Add files via upload

---
+++ paoloexample.c
@@ -0,0 +1,136 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+#define GETTIME
+
+
+
+#ifdef GETTIME
+#include <sys/time.h>
+struct timeval _t1,_t2;
+double duration;
+
+#define START_CLOCK   gettimeofday(&_t1,NULL ); 
+#define END_CLOCK   gettimeofday(&_t2,NULL);   duration = (_t2.tv_sec-_t1.tv_sec)+ (double)(_t2.tv_usec-_t1.tv_usec)/1000000;    printf("----------> get time %e sec<------\n",duration); 
+#endif /*  GETTIME */
+
+
+#ifdef CLOCK
+#include <time.h>
+clock_t _t1,_t2;
+double duration;
+
+#define START_CLOCK   _t1 = clock(); 
+#define END_CLOCK     _t2 = clock(); duration =  ((double)(_t2-_t1))/CLOCKS_PER_SEC; \
+  printf("clock time %e s \n", duration); 
+#endif
+
+//#define GRAPH_PATH 1
+static int DEBUG=0;
+
+#include <SparseBLAS.h>
+#include <sorting.h>
+#include <parsparsecoo.h>
+
+
+
+int main(int argc, char **argv) {
+  int k, D,Ps;
+  COO M,MT;
+  COO temp;
+  int ver;
+  int res;
+  Mat *C, *A, *B;
+
+  
+  printf("Size/dimension of a square matix K "); res= scanf("%d", &k);
+  printf("Degree or integer part of a percentage as density D "); res=scanf("%d", &D);
+  printf("Number of parallel processes P "); res=scanf("%d", &Ps);
+  
+  M = buildrandom_coo_list(k, D);
+  ver = validate(M);
+
+  if (!ver) {
+    return 2;
+  }
+      
+  
+  
+  printf("M %ld %d %d \n", M.length, M.M, M.N); 
+  
+  MT = M;
+
+  MT.data = (COOE*) malloc(M.length*sizeof(COOE));
+  
+  assert(MT.data);
+    
+  for (int i=0;i<M.length;i++) MT.data[i] = M.data[i];
+ //print_coo(MT);
+  START_CLOCK;
+  rowsort(&M); // we really transpose the data so that the order is
+  END_CLOCK;
+  //print_coo_c(MT);
+  
+  //print_coo(MT);
+  START_CLOCK;
+  columnsort(&MT); // we really transpose the data so that the order is
+  END_CLOCK;
+  //print_coo_c(MT);
+
+  ver = validateT(MT);
+  if (!ver) {
+    return 2;
+  }
+  
+  
+  printf("MT %ld %d %d \n", MT.length, MT.M, MT.N); 
+  
+  if (DEBUG) print_coo(M);
+
+  
+  START_CLOCK;
+  if (Ps<=1) { 
+    temp = matmul_coo(M,MT);
+    printf("## %2d %3d %6d ",  1, D,MT.M);
+  } 
+  else { 
+    temp = matmul_coo_par(M,M,MT,Ps);
+    printf("## %2d %3d %6d ",  Ps, D, MT.M);
+  }
+  END_CLOCK;
+
+  
+  
+  print_coo(temp);
+
+  
+  A = build_dense(M,1);
+  compare_dense(M,A);
+  
+  B = build_dense(MT,1);
+  C = build_dense(temp,0);
+  
+  matmul_f(C, temp.M, temp.N,
+	   A, M.M, M.N,
+	   B, MT.M, MT.N);
+
+
+  compare_dense(temp,C);
+
+  free(A);
+  free(B);
+  free(C);
+    
+  
+  free(MT.data);
+  free(M.data);
+  free(temp.data);
+
+  return 0;
+}
+
---
+++ parsparsecoo.c
@@ -0,0 +1,408 @@
+
+//#define GRAPH_PATH 1
+
+static int DEBUG = 0;
+static int DEBUG2=0;
+#include <SparseBLAS.h>
+
+
+
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sorting.h>
+
+#include <pthread.h>
+
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <assert.h>
+
+/*
+#define _GNU_SOURCE
+#include <sched.h>
+*/
+#define CPU_SETSIZE __CPU_SETSIZE
+# define CPU_ZERO(cpusetp) __CPU_ZERO_S (sizeof (cpu_set_t), cpusetp)
+# define CPU_SET(cpu, cpusetp) __CPU_SET_S (cpu, sizeof (cpu_set_t), cpusetp)
+# define CPU_ISSET(cpu, cpusetp) __CPU_ISSET_S (cpu, sizeof (cpu_set_t), cpusetp)
+# define CPU_COUNT(cpusetp)      __CPU_COUNT_S (sizeof (cpu_set_t), cpusetp)
+
+
+typedef COO  (*MatrixComputation)(COO A, COO B);
+typedef struct operands_addition TAddOperands;
+
+struct operands_addition { 
+  int  pi;
+  MatrixComputation m;  // C = A*B 
+  COO   *c;
+  COO   a;
+  COO   b;
+} ;
+
+
+void *basicComputation( void *s) {
+  TAddOperands mc = *(TAddOperands *)s;
+  int p1;
+  cpu_set_t mask;
+  if (mc.pi >= 0)  {
+
+    CPU_ZERO(&mask);
+    CPU_SET(mc.pi, &mask);
+
+    //p1 = sched_setaffinity(0,sizeof(mc.pi),&(mc.pi));
+    p1 = sched_setaffinity(0,sizeof(mask),&(mask));
+    if (p1<0) { 
+      printf(" Fail processor setting pt %d \n",mc.pi);
+    }
+  }
+  
+  *mc.c = mc.m(mc.a,mc.b);
+  printf("C =%2d  Ops %lu %ld x %ld x %ld \n",
+	 mc.pi,
+	 mc.c->ops,
+	 mc.c->length,
+	 mc.a.length,
+	 mc.b.length);
+  return 0;
+}
+
+
+void MatrixComputations(TAddOperands *args, int len)  {
+  
+  pthread_t*  p_thread; /* thread's structure */
+  pthread_attr_t attr;
+  int* thr_id;
+  int i;
+  int k=len;
+  
+  thr_id = malloc(k * sizeof(int) );
+  p_thread = malloc(k * sizeof(pthread_t) );
+  pthread_attr_init(&attr);
+
+
+  pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+  
+  for (i = 0; i<k-1; i++){
+    //printf("k %d \n",k);
+    thr_id[i] = pthread_create(&p_thread[i], 
+			       &attr, 
+			       basicComputation, 
+			       (void *)(args+i));
+  }
+
+  basicComputation((void *)(args+i));
+  
+ //START_CLOCK;
+ /* wait for the threads to complete */
+ for (i = 0; i<k-1; i++){
+   pthread_join(p_thread[i], NULL);
+ }
+ if (DEBUG2) printf(" Done pthreading \n");
+
+ free(thr_id);
+ free(p_thread);
+
+
+}
+
+
+
+
+COO merge( COO C, COO T) {  // R = C+T both sparse
+
+  COOTemporary TR= {NULL, 0, C.M, C.N};  
+  COO R = initialize_COO(NULL, 0, C.M, C.N);  
+  int i,j;
+  COOE c,t;
+  initialize_coot(&TR);          
+  long unsigned int ops=0;
+  
+  for (i=0, j =0; i<C.length && j<T.length; ){
+    c = C.data[i];
+    t = T.data[j];
+    if ((c.m*C.N+c.n)<(t.m*T.N+t.n)) {
+      append_coot(&TR,c);
+      i++;
+    } else if ((c.m*C.N+c.n)>(t.m*T.N+t.n)) {
+      append_coot(&TR,t);
+      j++;
+    } else {
+      ops++;
+      c.value = add(c.value,t.value);
+      append_coot(&TR,c);
+      i++;
+      j++;
+    }
+  }
+  for (; i<C.length ;i++ )
+    append_coot(&TR,C.data[i]);
+  
+  for (;  j<T.length; j++)
+    append_coot(&TR,T.data[j]);
+  
+
+  R.length = TR.length;
+  R.data = (COOE*) malloc(TR.length*sizeof(COOE));
+  R.ops = ops + T.ops;
+  for (int t=0; t<TR.length;t++)	{
+    R.data[t] = index_coot(&TR,t);
+  }
+  if (DEBUG) printf("Compressed  \n");
+  
+  free_coot(&TR);
+  
+  return R;
+}
+
+COO merge_alt( COO C, COO T) {  // R = C+T both sparse
+
+  COO TR= initialize_COO(NULL, 0, C.M, C.N);  
+  COO R;
+  int i,j,k;
+  COOE c,t;
+
+  TR.data = (COOE*) malloc((T.length+C.length)*sizeof(COO));
+  
+  k =0;
+  for (i=0, j =0; i<C.length && j<T.length; k++){
+    c = C.data[i];
+    t = T.data[j];
+    if ((c.m*C.N+c.n)<(t.m*T.N+t.n)) {
+      TR.data[k]=c;
+      i++;
+    } else if ((c.m*C.N+c.n)>(t.m*T.N+t.n)) {
+      TR.data[k]=t;
+      j++;
+    } else {
+      c.value = add(c.value,t.value);
+      TR.data[k]=c;
+      i++;
+      j++;
+    }
+  }
+  for (; i<C.length ;i++,k++ )
+    TR.data[k] = C.data[i];
+  
+  for (;  j<T.length; j++,k++)
+    TR.data[k] = T.data[j];
+  
+  
+  R.length = k;
+  R.data = (COOE*) malloc(TR.length*sizeof(COOE)); 
+  for (int t=0; t<k;t++)	{
+    R.data[t] = TR.data[t];
+  }
+  if (DEBUG) printf("Compressed merge all \n");
+	
+  free(TR.data);
+  
+  return R;
+}
+
+static inline int count_rows(COO A, int *b) {
+  int count; 
+  int i;
+  if (A.length<=0 ) return 0;
+
+
+  count = 0;
+  b[count] = 0; 
+  for (i=1; i<A.length; i++)
+    if (A.data[i-1].m != A.data[i].m) {
+      count ++;
+      b[count] = i;
+    }
+  count++;
+  b[count] = i;
+  return count;
+}
+
+
+
+
+COO *split_rows(COO A, int Ps) {
+
+  int *b;
+  COO *Rows;
+  int L = A.M;
+  int r;
+  int K, RK;
+  int k=0;
+  int i;
+
+  b = (int*) malloc((L+1)*sizeof(int));
+  Rows = (COO*) malloc(Ps*sizeof(COO));
+  
+  //if (DEBUG) printf("%d\n",(int)b); 
+  
+  r = count_rows(A,b);
+  
+  if (DEBUG) printf("L = %d-%dx%d r =%d \n",L,A.M,A.N,r); 
+  RK = r%Ps;
+  
+  K = r/(Ps) + ((RK>0)?1:0) ;
+
+  if (DEBUG) printf("Rows = %d K =%d RK =%d Ps=%d \n",r,K, RK,Ps);     
+  for (i=0; k<Ps-1;i+=K,k++) {
+    if (DEBUG) printf("k < %d i =%d b[i] = %d L = %d \n",k,i,b[i],b[i+K]-b[i]-1);     
+    Rows[k].data = A.data +b[i];
+    Rows[k].length = b[i+K]-b[i];
+    Rows[k].M = A.M;
+    Rows[k].N = A.N;
+  }
+  if (1) {
+    if (DEBUG) printf("k = %d i =%d b[i] = %d r =%d L = %d\n",k,i,b[i],r,b[r-1]-b[i]);     
+    Rows[k].data = A.data +b[i];
+    Rows[k].length = b[r-1]-b[i];
+    Rows[k].M = A.M;
+    Rows[k].N = A.N;
+  }  
+  if (DEBUG) printf("#Rows => %d \n",k);
+  //printf("%u\n",(unsigned int)b); 
+  free(b);
+
+
+  
+  return Rows;
+}
+
+
+
+
+COO matmul_coo_par(COO C,COO A,COO B,
+	       int Ps /* number of threads */
+	       ) {
+  long unsigned int ops = 0;
+  int i, j, k;
+  COO *Rows;
+  COO *Ts; 
+
+  COO TR = initialize_COO( NULL, 0, A.M, B.N);
+  COO R = initialize_COO( NULL, 0, C.M, C.N);
+  TAddOperands *args = (TAddOperands*) malloc(Ps*sizeof(TAddOperands));
+  
+  if (DEBUG2) printf("Parallel %d\n",Ps);
+
+  Rows = split_rows(A,Ps);
+  if (DEBUG2) printf("Rows \n");
+  Ts   = (COO*) calloc(Ps,sizeof(COO));
+  // This will be parallelized 
+  for (i=0;i<Ps;i++) {
+    
+    Ts[i].M = A.M; Ts[i].N = B.N; 
+    args[i].pi = i;
+    args[i].m = matmul_coo;
+    args[i].c = Ts + i;
+    args[i].a = Rows[i];
+    args[i].b = B;
+  }
+
+  MatrixComputations(args,Ps);
+  free(args);
+
+  // collecting theresults 
+  j = 0;
+  for (k=0,i=0;i<Ps;i++)  {
+    j += Ts[i].length;
+    ops += Ts[i].ops;
+    if (DEBUG) printf("Ts[%i] %d %d \n",i,Ts[i].M,Ts[i].N); 
+    if (DEBUG && !validate(Ts[i])) {
+      printf("Problems with T[%d]\n",i);
+    } 
+  }
+  TR.data = (COOE *) malloc(j*sizeof(COOE));
+  TR.length = j;
+  TR.ops = ops;
+  if (DEBUG2) printf("Combining %d\n",Ps);
+  for (k=0,i=0;i<Ps;i++) 
+    for (j=0; j< Ts[i].length; j++)
+      TR.data[k++] = Ts[i].data[j];
+
+  if (DEBUG) printf("TR %d %d %ld  \n",TR.M,TR.N,TR.length); 
+  if (DEBUG && !validate(TR)) {
+    
+    printf("Problems with TR\n");
+  }
+  if (DEBUG) printf("C %d %d \n",C.M,C.N); 
+  if (DEBUG && !validate(C)) {
+    printf("Problems with C before merge with TR\n");
+  } 
+  
+  for (k=0,i=0;i<Ps;i++) 
+    free(Ts[i].data);
+  free(Ts);
+  free(Rows);
+  
+  if ( DEBUG2) printf("Merging C and T  %d\n",Ps);
+  R = merge(C,TR);
+
+  if (DEBUG &&  !validate(R)) {
+    printf("Problems with R\n");
+  } 
+  
+
+  
+  if (DEBUG2) printf("last free \n");
+  free(TR.data);
+  
+  return R;
+}
+
+COO
+matmul_coo_par_basic(
+		     int *_CX, int *_CY, Mat *_CV,
+		     long unsigned int LC,int MC, int NC,
+		     int *_AX,int *_AY,  Mat *_AV,
+		     long unsigned int LA, int MA,int NA,
+		     int *_BX, int *_BY, Mat *_BV,
+		     long unsigned int LB, int MB, int NB,
+		     int Ps /* number of threads */
+		     ) {
+  printf("matmul_coo_par_basic \n");
+  if (DEBUG2) printf("C %lu \n", LC);
+  COOE *_C = from_three_to_one(_CX,_CY,_CV,LC);
+  if (DEBUG2) printf("A %lu \n", LA);
+  COOE *_A = from_three_to_one(_AX,_AY,_AV,LA);;
+  if (DEBUG2) printf("B %lu \n",LB);
+  COOE *_B = from_three_to_one(_BX,_BY,_BV,LB);; 
+  COO C = initialize_COO( _C, LC, MC, NC);
+  COO A = initialize_COO( _A, LA, MA, NA);
+  COO B = initialize_COO( _B, LB, MB, NB);
+  COO R;
+
+  columnsort(&B); // we really transpose the data so that the order is  
+
+
+  if (!validate(C)) {
+    printf("Problems with C\n");
+  } 
+  if (!validate(A)) {
+    printf("Problems with A\n");
+  }
+  if (!validateT(B)) {
+    printf("Problems with B\n");
+  }
+  
+  R = matmul_coo_par(C,A,B,Ps);
+
+  if (!validate(R)) {
+    printf("Problems with R\n");
+  } 
+
+
+  if (DEBUG2) printf("Done matmul_coo_par\n");
+  free(_C);
+  free(_A);
+  free(_B);
+  // This has to be free by who calls this 
+  return R;
+}
+  
+  
+  
+  
+  
